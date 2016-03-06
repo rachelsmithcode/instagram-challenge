441d01366cd3fdfb8268221af1c2ff26bf388056
rachelsmithcode <rachelcodesmith@outlook.com>
2016-03-06 22:09:44 +0000

    user signin options added via devise

--- instagram_challenge/config/routes.rb
+++ instagram_challenge/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'photos' => 'photos#index'
   resources :photos do
     resources :comments
   end
+
+  root to: "photos#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
